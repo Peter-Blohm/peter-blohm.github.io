41443faf042f149a6392597d516500f51b474f36
Peter-Blohm <35277333+Peter-Blohm@users.noreply.github.com>
2026-03-19 15:51:19 +0000

    Deploying to gh-pages from @ Peter-Blohm/peter-blohm.github.io@ba18900699df3a22a610528be1bd083f2017ba22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-my-first-post",
         
           title: "My first post",
